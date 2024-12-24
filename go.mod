f17c4669680cf4a172781a7975d9256356b81af9
Thejus Rajendran <thejusr1999@icloud.com>
2024-12-24 23:31:42 +0530

    added storage layer for posts and users

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/thejus-r/social
 
 go 1.23.4
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require github.com/go-chi/chi/v5 v5.2.0
+
+require github.com/lib/pq v1.10.9
---
+++ package/store/posts.go
@@ -0,0 +1,46 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+
+	pg "github.com/lib/pq"
+)
+
+type Post struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserId    int64    `json:"user_ID"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
+type PostStore struct {
+	db *sql.DB
+}
+
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
+
+	query := `
+	INSERT INTO posts (content, title, user_id, tags)
+	VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Content,
+		post.Title,
+		post.ID,
+		pg.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
