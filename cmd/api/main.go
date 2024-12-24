402999e9c9fa5f1584b934fd7ed7dc9aea7a8b6e
Thejus Rajendran <thejusr1999@icloud.com>
2024-12-24 23:42:57 +0530

    added db & storage layer configs to application iterface

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,17 +3,41 @@ package main
 import (
 	"log"
 
+	"github.com/thejus-r/social/package/db"
 	"github.com/thejus-r/social/package/env"
+	"github.com/thejus-r/social/package/store"
 )
 
 func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/tiny_ledger?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(
+		cfg.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Print("DB Connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
