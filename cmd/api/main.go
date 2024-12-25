b6c1dffe0f7badaa1e0a824241bf5d002dfe169f
Thejus Rajendran <thejusr1999@icloud.com>
2024-12-25 20:08:00 +0530

    handling errors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/thejus-r/social/package/store"
 )
 
+const version = "0.0.1"
+
 func main() {
 
 	cfg := config{
@@ -18,6 +20,7 @@ func main() {
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		env: env.GetString("ENV", "development"),
 	}
 
 	db, err := db.New(
